50947b6025b3d7a76f9041b0e3a1e8d50a3d3e5c
Juan <juansoussev@gmail.com>
2023-06-01 23:08:47 -0300

    2023

---
+++ 2023/leilao.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+
+max_lance = 0
+winner_name = ""
+
+if 0 <= n <= 10000:
+    for i in range(n):
+
+        name = input()
+        value = int(input())
+
+        if 1 <= value <= 100000 and 1 <= len(name) <= 10:
+            if value > max_lance:
+                max_lance = value
+                winner_name = name
+        else:
+            print('invalid')
+else:
+    print('invalid')
+
+
+print(winner_name)
+print(max_lance)
